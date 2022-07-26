e02c804c047ac872e7b9909ce8bbe9d19f4ec8f9
Swaniket <swaniketchowdhury@gmail.com>
2022-07-26 09:24:55 +0530

    Completed Feedback Form & Form Validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 import Header from "./componets/Header";
 import FeedbackList from "./componets/FeedbackList";
 import FeedbackStats from "./componets/FeedbackStats";
 import FeedbackForm from "./componets/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./componets/AboutIconLink";
+// import Card from "./componets/shared/Card";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -15,15 +25,46 @@ function App() {
     }
   };
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   return (
-    <>
+    <Router>
       <Header text="Feedback UI" />
       <div className="container">
-        <FeedbackForm />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+                <AboutIconLink />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+
+        {/* We can use this in the Navigation Bar */}
+        {/* <Card>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink to="/about" activeClassName="active">
+            About
+          </NavLink>
+        </Card> */}
       </div>
-    </>
+    </Router>
   );
 }
 
--- src/componets/FeedbackForm.jsx
+++ src/componets/FeedbackForm.jsx
@@ -1,32 +1,48 @@
 import { useState } from "react";
+import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
-    if (text === '') {
-        setBtnDisabled(true)
-        setMessage(null)
-    } else if (text !== '' && text.trim().length <= 10) {
-        setMessage("Review must me atleast 10 characters")
-        setBtnDisabled(true)
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Review must me atleast 10 characters");
+      setBtnDisabled(true);
     } else {
-        setMessage(null)
-        setBtnDisabled(false)
+      setMessage(null);
+      setBtnDisabled(false);
     }
     setText(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+
+      handleAdd(newFeedback);
+      setText('')
+    }
+  };
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>
           <strong>How would you rate your experience with us?</strong>
         </h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             type="text"
